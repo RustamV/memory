2689ec1755a09414df864368493d98a2c099b761
RustamV <rvrd1996@gmail.com>
2021-10-13 12:23:12 +0300

    prettier & readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>123</div>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div>123</div>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
